16532891c81943357f5222399a51023098d9ceb1
jmawanzi <165791421+jmawanzi@users.noreply.github.com>
2024-04-12 16:37:23 +0200

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -5,15 +5,10 @@ connection = sqlite3.connect('database2.db')
 with open('schema2.sql') as f:
     connection.executescript(f.read())
 
-cursor = connection.cursor()
+cur = connection.cursor()
 
 cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(1, 'Emilie', 'Victor', 2024, 10))
 cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(2, 'Didier', 'Laurent', 2023, 5))
-cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(3, 'Georges', 'Hugo', 2020, 3))
-cur.execute("INSERT INTO Livres (ID_livre, Titre, Auteur, Annee_publication, Quantite) VALUES (?, ?, ?, ?, ?)",(2, 'Lars', 'Sullivan', 2021, 6))
 
 connection.commit()
 connection.close()
-
-if __name__ == "__main__":
-    create_database()
