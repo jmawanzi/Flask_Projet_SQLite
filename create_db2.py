839173c6084f6d707ee2f0e79cc85a9555e30572
jmawanzi <165791421+jmawanzi@users.noreply.github.com>
2024-04-12 15:20:21 +0200

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -8,34 +8,13 @@ def create_database():
     cursor = conn.cursor()
 
     # Création de la table des Livres
-    cursor.execute('''CREATE TABLE IF NOT EXISTS Livres (
-                       livre_id INTEGER PRIMARY KEY,
-                       titre TEXT NOT NULL,
-                       auteur TEXT,
-                       genre TEXT,
-                       stock INTEGER
-                       )''')
+    cursor.execute("CREATE TABLE IF NOT EXISTS Livres (livre_id INTEGER PRIMARY KEY, titre TEXT NOT NULL, auteur TEXT, genre TEXT, stock INTEGER)")
 
     # Création de la table des Utilisateurs
-    cursor.execute('''CREATE TABLE IF NOT EXISTS Utilisateurs (
-                       utilisateur_id INTEGER PRIMARY KEY,
-                       nom TEXT NOT NULL,
-                       prenom TEXT NOT NULL,
-                       email TEXT UNIQUE NOT NULL,
-                       role TEXT NOT NULL
-                       )''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Utilisateurs (utilisateur_id INTEGER PRIMARY KEY, nom TEXT NOT NULL, prenom TEXT NOT NULL, email TEXT UNIQUE NOT NULL, role TEXT NOT NULL)''')
 
     # Création de la table des Emprunts
-    cursor.execute('''CREATE TABLE IF NOT EXISTS Emprunts (
-                       emprunt_id INTEGER PRIMARY KEY,
-                       utilisateur_id INTEGER,
-                       livre_id INTEGER,
-                       date_emprunt DATE,
-                       date_retour_prevue DATE,
-                       date_retour_effective DATE,
-                       FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(utilisateur_id),
-                       FOREIGN KEY (livre_id) REFERENCES Livres(livre_id)
-                       )''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Emprunts (emprunt_id INTEGER PRIMARY KEY, utilisateur_id INTEGER, livre_id INTEGER, date_emprunt DATE, date_retour_prevue DATE, date_retour_effective DATE, FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(utilisateur_id), FOREIGN KEY (livre_id) REFERENCES Livres(livre_id))''')
 
     # Commit des changements et fermeture de la connexion
     conn.commit()
